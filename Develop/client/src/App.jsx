e10b7163ae72f015c9aa3e9a77b4e923b6b7058c
Samira Chetta <chettasamira@gmail.com>
2024-03-16 00:23:05 -0400

    Refactored App.jsx with ApolloProvider

--- Develop/client/src/App.jsx
+++ Develop/client/src/App.jsx
@@ -1,14 +1,44 @@
 import './App.css';
 import { Outlet } from 'react-router-dom';
-
+import SearchBooks from "./pages/SearchBooks";
+import SavedBooks from "./pages/SavedBooks";
 import Navbar from './components/Navbar';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+// GraphQL endpoint
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+// Middleware
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  // Executes auth in middleware before making any request to GraphQL
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <>
+  <ApolloProvider client={client}>
       <Navbar />
       <Outlet />
-    </>
+      </ApolloProvider>
   );
 }
 
