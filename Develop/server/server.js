425a97edb075ccfbe8f072926da47a5322a8fe9b
Samira Chetta <chettasamira@gmail.com>
2024-03-16 14:09:51 -0400

    Refactored auth.js and server.js

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -1,4 +1,3 @@
-require('dotenv').config();
 const express = require('express');
 const path = require('path');
 // Import ApolloServer
@@ -8,9 +7,19 @@ const { authMiddleware } = require("./utils/auth");
 // Import typeDefs and resolvers from schema
 const { typeDefs, resolvers } = require("./schemas");
 const db = require('./config/connection');
-const routes = require('./routes');
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+// Create a Apollo server and pass in schema data
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  // Context used for token authentication
+  context: authMiddleware,
+});
+
+// Integrate Apollo server with Express application as middleware
+server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -20,7 +29,10 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
+
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
